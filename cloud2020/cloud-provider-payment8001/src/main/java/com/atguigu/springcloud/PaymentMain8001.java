1ab51e957bb84c42c49992979a7e82905532efcf
haishaoa <haishaoa@foxmail.com>
2020-08-04 17:16:35 +0800

    Eureka单机版

--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author haishao
  * @create 2020-08-03-21:57
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args){
         SpringApplication.run(PaymentMain8001.class,args);
